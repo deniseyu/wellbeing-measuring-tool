9d1ba3ce241c6882464bbb33e6d4870017fd0f38
Denise <yu.denise.d@gmail.com>
2015-04-26 22:27:31 +0100

    add validations to participant, ability to remove participants from study

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,29 +1,38 @@
 class ParticipantsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_study
 
   def index
-    @study = Study.find(params[:study_id])
   end
 
   def new
-    @study = Study.find(params[:study_id])
     @participant = Participant.new
   end
 
   def create
-    @study = Study.find(params[:study_id])
     @participant = @study.participants.new(participant_params)
 
     if @study.save
       ParticipantMailer.intro_email(@participant, @study).deliver
       redirect_to study_path(@study)
     else
-      flash[:notice] = 'Sorry, something went wrong. Please make sure email and phone number are valid.'
+      flash[:notice] = 'Sorry, something went wrong. Please make sure all required fields are filled out.'
       redirect_to new_study_participant_path
     end
   end
 
+  def destroy
+    participant = Participant.find(params[:id])
+    participant.destroy
+    flash[:notice] = 'Participant successfully removed.'
+    redirect_to study_participants_path(@study)
+  end
+
   protected
+
+  def set_study
+    @study = Study.find(params[:study_id])
+  end
 
   def participant_params
     params.require(:participant).permit(:first_name, :last_name, :email, :phone, :notes)
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,3 +1,8 @@
 class Participant < ActiveRecord::Base
   has_many :responses
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email
+  validates_presence_of :phone
 end
