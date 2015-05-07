239b3d1d4e426122e41d3eb60dfe673359b4d5d4
Denise <yu.denise.d@gmail.com>
2015-05-07 20:38:18 +0100

    Sendgrid configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,11 +47,17 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'http://wellbeing-demo.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'wellbeing-demo.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    # address: 
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
   # Use a different logger for distributed setups.
